937921a8d6ece0d33b6b2bf112fbd8e4ae2fb723
hasan akcakoca <hasanakcakoca@gmail.com>
2023-10-07 14:43:50 +0300

    update

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -9,14 +9,28 @@ namespace UdemyRabbitMQ.publisher
     {
         static void Main(string[] args)
         {
+            /*
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://uhshoatb:4tRfDsemduk6BCrsZaIvfQgOhLsMtf-t@fish.rmq.cloudamqp.com/uhshoatb");
+            */
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "192.168.1.109",
+                UserName = "my",
+                Password = "1",
+                Port = 5672
+            };
+
 
             using var connection = factory.CreateConnection();
 
             var channel = connection.CreateModel();
 
-            channel.QueueDeclare("hello-queue", true, false, false);
+           // channel.QueueDeclare("hello-queue", true, false, false);
+           //yada
+            channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false);
+
 
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
@@ -25,7 +39,9 @@ namespace UdemyRabbitMQ.publisher
 
                 var messageBody = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+               // channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+               //yada
+                channel.BasicPublish( exchange: string.Empty,routingKey: "hello-queue", basicProperties: null,body: messageBody);
 
                 Console.WriteLine($"Mesaj gönderilmiştir : {message}");
 
