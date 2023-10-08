40056fcf26114800741ad4d6eccccf3393f82cc0
hasan akcakoca <hasanakcakoca@gmail.com>
2023-10-08 16:15:34 +0300

    yorum

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -18,50 +18,64 @@ namespace UdemyRabbitMQ.publisher
     {
         static void Main(string[] args)
         {
+            // RabbitMQ bağlantı ayarlarını yapılandırma
+            /*
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://uhshoatb:4tRfDsemduk6BCrsZaIvfQgOhLsMtf-t@fish.rmq.cloudamqp.com/uhshoatb");
+            */
+            // RabbitMQ sunucusuna bağlanmak için ConnectionFactory oluşturulur.
+            var factory = new ConnectionFactory
+            {
+                HostName = "192.168.1.109",
+                UserName = "my",
+                Password = "1",
+                Port = 5672
+            };
 
+
+
+            // RabbitMQ'ya bağlantı açma
             using var connection = factory.CreateConnection();
-
             var channel = connection.CreateModel();
 
+            // Exchange oluşturma (direct tipinde ve kalıcı)
             channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
 
-
+            // LogNames enumundaki her bir log tipi için kuyruk oluşturma ve bind işlemi yapma
             Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
             {
                 var routeKey = $"route-{x}";
                 var queueName = $"direct-queue-{x}";
-                channel.QueueDeclare(queueName, true, false, false);
 
-                channel.QueueBind(queueName, "logs-direct",routeKey,null);
+                //kuyruğu oluşturur veya var olan bir kuyruğa erişim sağlar
+                channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+                // channel.QueueDeclare(queueName, true, false, false);
+
+
+
+
+                // Oluşturulan kuyruğu ve exchange'i routeKey ile bind etme
+                // channel.QueueBind(queueName, "logs-direct", routeKey, null);
+                channel.QueueBind(queue: queueName,exchange: "logs-direct",routingKey: routeKey,arguments: null);
+
 
             });
 
-
-
+            // Rastgele log mesajları üreterek ilgili kuyruklara gönderme
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-
                 LogNames log = (LogNames)new Random().Next(1, 5);
-
                 string message = $"log-type: {log}";
-
                 var messageBody = Encoding.UTF8.GetBytes(message);
-
                 var routeKey = $"route-{log}";
 
-                channel.BasicPublish("logs-direct",routeKey, null, messageBody);
-
+                // Mesajı ilgili exchange'e ve routeKey'e gönderme
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
                 Console.WriteLine($"Log gönderilmiştir : {message}");
-
             });
 
-
-
             Console.ReadLine();
-
-
         }
     }
+
 }
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -11,41 +11,49 @@ namespace UdemyRabbitMQ.subscriber
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqps://uhshoatb:4tRfDsemduk6BCrsZaIvfQgOhLsMtf-t@fish.rmq.cloudamqp.com/uhshoatb");
+            // RabbitMQ bağlantı ayarlarını yapılandırma
+            var factory = new ConnectionFactory
+            {
+                HostName = "192.168.1.109",
+                UserName = "my",
+                Password = "1",
+                Port = 5672
+            };
 
+            // RabbitMQ'ya bağlantı açma
             using var connection = factory.CreateConnection();
-
             var channel = connection.CreateModel();
-            
 
-            channel.BasicQos(0, 1, false);
-            var consumer = new EventingBasicConsumer(channel);
-
+            // Kuyruk ismi
             var queueName = "direct-queue-Critical";
-            channel.BasicConsume(queueName,false, consumer);
+
+            // Consumer'a sadece bir mesajı işleme yetkisi ver (prefetchCount: 1)
+            channel.BasicQos(0, 1, false);
+
+            // Belirtilen kuyruğu dinleyen EventingBasicConsumer oluşturma
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName, false, consumer);
 
             Console.WriteLine("Logları dinleniyor...");
 
+            // Mesaj geldiğinde çalışacak event handler
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
+                // Mesajı işleme simülasyonu (Thread.Sleep ile 1.5 saniye bekleme)
                 Thread.Sleep(1500);
                 Console.WriteLine("Gelen Mesaj:" + message);
 
-               // File.AppendAllText("log-critical.txt", message+ "\n");
-
-                channel.BasicAck(e.DeliveryTag, false);
+                // Mesaj başarıyla işlendi, acknowledgment gönderme
+                //Bu şekilde, mesajlar doğru bir şekilde işlenene kadar yeni mesajlar alınmaz, böylece işlem sırasında hataların oluşması engellenir.
+                channel.BasicAck(e.DeliveryTag,multiple: false);
             };
-
-
-
-           
 
             Console.ReadLine();
         }
 
-      
+
+
     }
 }
